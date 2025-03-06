41b95a33736957f070e1f99c9306bae1051b9b36
Hrvoje Knezevic <Hrvoje.Knezevic@andritz.com>
2025-03-06 18:53:48 +0100

    Calculated array of integers.

--- GitAdfInfo/Program.cs
+++ GitAdfInfo/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using GitAdfInfo.Services;
+
 Console.WriteLine("ADF INFO");
+
+var service = new NumberService();
+var result = service.CalculateNumbers([1, 2, 3, 4, 5]);
+Console.WriteLine($"Sum: {result.Sum}");
