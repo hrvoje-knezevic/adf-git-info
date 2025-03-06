04e15fc3faffd493556c53768f26ad8a718d25e8
Hrvoje Knezevic <Hrvoje.Knezevic@andritz.com>
2025-03-06 20:47:25 +0100

    New text service

---
+++ GitAdfInfo/Services/TextService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitAdfInfo.Services
+{
+    public class TextService
+    {
+        public string ReverseString(string input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentException("The input string must not be null.");
+            }
+
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
