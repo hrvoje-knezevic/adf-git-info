3c5d72510783090f33ba01e56994f7d8a60ff05a
Hrvoje Knezevic <Hrvoje.Knezevic@andritz.com>
2025-03-07 14:24:54 +0100

    Completely new

--- GitAdfInfo/Program.cs
+++ GitAdfInfo/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using GitAdfInfo.Services;
 
-Console.WriteLine("ADF INFO");
+Console.WriteLine("ADF INFO 123");
 
 var service = new NumberService();
 var result = service.CalculateNumbers([1, 2, 3, 4, 5]);
